7b981c6c16a1301dc377aa6145a07b14a83a48f5
bijan102 <bfazeli1@gmail.com>
2016-11-23 01:20:56 -0800

    Implemented HodDogStand class.

--- Project/HotDogStand.cpp
+++ Project/HotDogStand.cpp
@@ -1,3 +1,41 @@
+#include "HotDogStand.h"
 
+int HotDogStand::total = 0;
 
+HotDogStand::HotDogStand()
+{
+	numSold = 0;
+	id = 0;
+}
 
+void HotDogStand::setID(int id)
+{
+	this->id = id;
+}
+
+int HotDogStand::getID() const
+{
+	return id;
+}
+
+int HotDogStand::getStandSales() const
+{
+	return numSold;
+}
+
+void HotDogStand::justSold()
+{
+	++numSold;
+	++total;
+}
+
+int HotDogStand::getAllSales()
+{
+	return total;
+}
+
+HotDogStand::~HotDogStand()
+{
+	total -= numSold;
+	numSold = 0;
+}
